faf351b92505a33edf0479aba1af0e4f41da3343
Alexey Pitinov <alexey.pitinov@ironwaterstudio.com>
2020-08-23 17:45:38 +0300

    add swap numbers

--- javaexamples/src/main/java/com/example/javaexamples/JavaExamples.java
+++ javaexamples/src/main/java/com/example/javaexamples/JavaExamples.java
@@ -2,7 +2,8 @@ package com.example.javaexamples;
 
 public class JavaExamples {
 	public static void main(String[] args) {
-		swapNumbers(4, 6);
+		//swapNumbers(4, 6);
+		sumAndSubNumbers(  6,  4);
 	}
 
 	public static void swapNumbers(int x, int y) {
@@ -16,6 +17,11 @@ public class JavaExamples {
 	}
 
 	public static void sumAndSubNumbers(int x, int y) {
-
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		int z = x + y;
+		int w = x - y;
+		System.out.println(z);
+		System.out.println(w);
 	}
 }
